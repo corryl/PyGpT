5e9c903d241ad48e31070f8de858d021df68e777
Giacinto Tripodi <giacintotripodi@gmail.com>
2023-02-10 10:25:29 +0100

    Added features to show conversation history and clear history.

--- pygpt.py
+++ pygpt.py
@@ -1,40 +1,52 @@
 import openai
-from pyfiglet import Figlet
+import pyfiglet
 
-custom_fig = Figlet(font='alligator2')
-print(custom_fig.renderText('PyGpt'))
+
+ascii_banner = pyfiglet.figlet_format("PyGpt")
+print(ascii_banner)
 print("By CorryL\n")
 
 # Define OpenAI API key
 openai.api_key = ""
 
-# Set up the model and prompt
-model_engine = "text-davinci-003"
-history = []
+conversation_history = []
 
-def chatgpt_clone(input, history):
-    history = history or []
-    s = list(sum(history, ()))
-    s.append(input)
-    inp = ' '.join(s)
-    completion = openai.Completion.create(
-        engine=model_engine,
-        prompt=inp,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    output = completion.choices[0].text
-    history.append((input, output))
-    return history, output
+end_string = "exit"
+clear_history_string = "clear"
+print_history_string = "history"
 
-# Generate a response
+print("\nexit - exit the application")
+print("history - show the entire conversation")
+print("clear - clear history\n")
+
 while True:
-    try:
-        prompt = input("\033[31mHuman: \033[0m")
-        history, response = chatgpt_clone(prompt, history)
-        print("\033[32mGpt: \033[0m" + response)
-    except Exception as e:
-        print("Errore:", e)
-        continue
+    user_input = input(">> ")
+
+    if user_input.strip().lower() == end_string:
+        print("Bye!")
+        break
+
+    elif user_input.strip().lower() == clear_history_string:
+        conversation_history = []
+        print("History cleared!")
+
+    elif user_input.strip().lower() == print_history_string:
+        for i in conversation_history:
+            print(i)
+
+    else:
+        conversation_history.append(f"You: {user_input}")
+
+        conversation_context = '\n'.join(conversation_history[-5:])
+
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"{user_input}\n{conversation_context}",
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        ).choices[0].text
+
+        print(response)
+        conversation_history.append(response)
